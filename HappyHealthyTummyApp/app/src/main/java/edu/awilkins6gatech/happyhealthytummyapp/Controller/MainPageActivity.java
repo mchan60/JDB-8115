11ed229a011b86e2f23078c02516eff80b035e73
Alexander Wilkins <awilkins6@gatech.edu>
2018-09-06 17:10:02 -0400

    commented out lines so it will run

--- HappyHealthyTummyApp/app/src/main/java/edu/awilkins6gatech/happyhealthytummyapp/Controller/MainPageActivity.java
+++ HappyHealthyTummyApp/app/src/main/java/edu/awilkins6gatech/happyhealthytummyapp/Controller/MainPageActivity.java
@@ -15,9 +15,9 @@ import edu.awilkins6gatech.happyhealthytummyapp.R;
 public class MainPageActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+//    static {
+//        System.loadLibrary("native-lib");
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +37,7 @@ public class MainPageActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //tv.setText(stringFromJNI());
     }
 
     @Override
@@ -66,5 +66,5 @@ public class MainPageActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    //public native String stringFromJNI();
 }
